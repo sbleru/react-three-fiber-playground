bfd311b28de7fe96237edfb833ce5dc9e2f6019a
sbleru <usareru@gmail.com>
2019-12-04 22:51:35 +0900

    refactor: divide into components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 /** @jsx jsx */
-import React, { useRef } from 'react';
-import { Canvas, useFrame } from 'react-three-fiber'
 import { css, jsx, Global } from '@emotion/core';
-import emotionReset from "emotion-reset";
+import emotionReset from 'emotion-reset';
+import { Work } from './containers/work';
 
 const globalStyles = css`
     ${emotionReset}
@@ -14,45 +13,11 @@ const globalStyles = css`
     }
 `;
 
-const theme = css`
-    width: 100vw;
-    height: 100vh;
-    background-color: #000;
-`;
-
-const App: React.FC = () => {
-  return (
+const App = () => (
     <div>
-      <Global styles={globalStyles} />
-      <div css={theme}>
-        <Canvas>
-          <Thing />
-        </Canvas>
-      </div>
+        <Global styles={globalStyles} />
+        <Work />
     </div>
-  );
-}
-
-const Thing = () => {
-  const ref: any = useRef()
-  useFrame(() => (ref.current.rotation.x = ref.current.rotation.y += 0.01))
-  return (
-    <mesh
-      ref={ref}
-      onClick={e => console.log('click')}
-      onPointerOver={e => console.log('hover')}
-      onPointerOut={e => console.log('unhover')}>
-      {/* <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
-      <meshNormalMaterial attach="material" /> */}
-      <planeBufferGeometry attach='geometry' args={[1, 1]} />
-      <meshBasicMaterial
-          attach='material'
-          color='hotpink'
-          opacity={0.5}
-          transparent
-      />
-    </mesh>
-  )
-}
+);
 
 export default App;
