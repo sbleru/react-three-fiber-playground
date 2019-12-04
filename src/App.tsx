a07d221d82379d8b61fd0674d8143d377a32c281
sbleru <usareru@gmail.com>
2019-12-04 22:47:10 +0900

    feat: add emotion style / reset style

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,35 @@
+/** @jsx jsx */
 import React, { useRef } from 'react';
-import logo from './logo.svg';
-import './App.css';
 import { Canvas, useFrame } from 'react-three-fiber'
+import { css, jsx, Global } from '@emotion/core';
+import emotionReset from "emotion-reset";
+
+const globalStyles = css`
+    ${emotionReset}
+    *, *::after, *::before {
+        box-sizing: border-box;
+        -moz-osx-font-smoothing: grayscale;
+        -webkit-font-smoothing: antialiased;
+        font-smoothing: antialiased;
+    }
+`;
+
+const theme = css`
+    width: 100vw;
+    height: 100vh;
+    background-color: #000;
+`;
 
 const App: React.FC = () => {
   return (
-    <Canvas>
-      <Thing />
-    </Canvas>
+    <div>
+      <Global styles={globalStyles} />
+      <div css={theme}>
+        <Canvas>
+          <Thing />
+        </Canvas>
+      </div>
+    </div>
   );
 }
 
@@ -20,8 +42,15 @@ const Thing = () => {
       onClick={e => console.log('click')}
       onPointerOver={e => console.log('hover')}
       onPointerOut={e => console.log('unhover')}>
-      <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
-      <meshNormalMaterial attach="material" />
+      {/* <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
+      <meshNormalMaterial attach="material" /> */}
+      <planeBufferGeometry attach='geometry' args={[1, 1]} />
+      <meshBasicMaterial
+          attach='material'
+          color='hotpink'
+          opacity={0.5}
+          transparent
+      />
     </mesh>
   )
 }
