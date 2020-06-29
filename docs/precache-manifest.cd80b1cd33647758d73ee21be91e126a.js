33d292578d8eb46468b2fe11caf69678e8fe4364
sbleru <usareru@gmail.com>
2020-06-30 01:35:32 +0900

    feat: build page

---
+++ docs/precache-manifest.cd80b1cd33647758d73ee21be91e126a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06f0aa466480562eb241d17a600d7237",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8dbb89dc6a7b8201b5ec",
+    "url": "/static/js/2.87c37f67.chunk.js"
+  },
+  {
+    "revision": "41d84e08fb4331e723b7",
+    "url": "/static/js/main.c8b64d5f.chunk.js"
+  },
+  {
+    "revision": "3b5475f9cd8cdd9600ac",
+    "url": "/static/js/runtime-main.442af73a.js"
+  }
+]);
